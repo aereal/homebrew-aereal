db06486fb2944ee30a1e5f1bffa7ffe9c8deac5c
aereal <aereal@aereal.org>
2012-12-31 02:13:39 +0900

    Fix sha1

--- perlbrew.rb
+++ perlbrew.rb
@@ -4,7 +4,7 @@ class Perlbrew < Formula
   homepage 'http://perlbrew.pl'
   url 'https://github.com/gugod/App-perlbrew/zipball/release-0.58'
   version '0.58'
-  sha1 'ceabbee2c4793afd29e227b8f08721dff003033b'
+  sha1 '4b98c8509f65a9ffe665afdf987109839c93e47e'
 
   head 'git://github.com/gugod/App-perlbrew.git', :branch => 'master'
 
