6e8d9f4224a8644615f3c2ef66ac231a353e3e87
aereal <aereal@aereal.org>
2013-11-05 01:10:54 +0900

    Fix the problem with AquaSKK's skkserv

--- uim.rb
+++ uim.rb
@@ -9,6 +9,10 @@ class Uim < Formula
   depends_on 'intltool' => :build
   depends_on 'gettext'
 
+  def patches
+    DATA
+  end
+
   def install
     ENV.append 'CFLAGS', '-g -O0'
     ENV.append 'CXXFLAGS', '-g -O0'
@@ -20,3 +24,26 @@ class Uim < Formula
     system "make install"
   end
 end
+
+__END__
+diff --git a/uim/skk.c b/uim/skk.c
+index b2931c1..a3b044a 100644
+--- a/uim/skk.c
++++ b/uim/skk.c
+@@ -808,7 +808,7 @@ search_line_from_server(dic_info *di, const char *s, char okuri_head)
+ 
+   uim_asprintf(&idx, "%s%c", s, okuri_head);
+ 
+-  fprintf(wserv, "1%s \n", idx);
++  fprintf(wserv, "1%s ", idx);
+   ret = fflush(wserv);
+   if (ret != 0 && errno == EPIPE) {
+     free(idx);
+@@ -857,6 +857,7 @@ search_line_from_server(dic_info *di, const char *s, char okuri_head)
+   } else {
+     while ((nr = read(skkservsock, &r, 1)) != -1 && nr != 0 && r != '\n')
+       ;
++    skkserv_disconnected(di); // disconnect from skkserv to re-sync
+     free(line);
+     return NULL;
+   }
